d8ad34a00b49e3b857d7ce72be1271bab7cc6794
Nzeako Kosisochukwu <nzeakokosi7@gmail.com>
2019-07-07 18:16:05 +0100

    Added test cases for GET /todo:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -66,5 +70,34 @@ describe('GET /todos', () => {
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
-    })
+    });
+});
+
+describe('Get /todos/:id', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 400 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(400)
+            .end(done);
+    });
+
+
+    it('should return 400 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123jh')
+            .expect(400)
+            .end(done);
+    });
 })
